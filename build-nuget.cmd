d49fa9a437b79025bfee4837aab57f33fa2c5ffa
JMacro <JMacro_H@163.com>
2020-05-18 21:38:55 +0800

    feat:完善打包命令

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -1,7 +1,17 @@
 set workDir=%cd%
-set PROJECT_PATH=%workDir%\Flash.LoadBalancer
-echo %PROJECT_PATH%
-
-dotnet pack %PROJECT_PATH% --output %workDir%\nupkgs
-start explorer %workDir%\nupkgs
+set nupkgs_path=%workDir%\nupkgs
+echo "Building..."
+dotnet pack %workDir%\Flash.Core --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.Cache --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.Cache.Redis --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.CQRS --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.Middlewares --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.OpenTracting --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.RabbitMQ --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.Security --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.System --output %nupkgs_path%
+dotnet pack %workDir%\Flash.Extersions.UidGenerator --output %nupkgs_path%
+dotnet pack %workDir%\Flash.LoadBalancer --output %nupkgs_path%
+start explorer %nupkgs_path%
+echo "End"
 pause
