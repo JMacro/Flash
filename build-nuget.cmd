03b92c46d80a81d660acc745c0786f8cef2ddadd
林展鸿 <JMacro_H@163.com>
2020-05-19 09:37:34 +0800

    feat:优化打包脚本

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -1,17 +1,9 @@
 set workDir=%cd%
 set nupkgs_path=%workDir%\nupkgs
 echo "Building..."
-dotnet pack %workDir%\Flash.Core --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.Cache --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.Cache.Redis --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.CQRS --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.Middlewares --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.OpenTracting --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.RabbitMQ --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.Security --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.System --output %nupkgs_path%
-dotnet pack %workDir%\Flash.Extersions.UidGenerator --output %nupkgs_path%
-dotnet pack %workDir%\Flash.LoadBalancer --output %nupkgs_path%
+for /d %%i in (Flash.*) do (
+	dotnet pack %workDir%\%%i --output %nupkgs_path%	
+)
+echo "Build End"
 start explorer %nupkgs_path%
-echo "End"
 pause
