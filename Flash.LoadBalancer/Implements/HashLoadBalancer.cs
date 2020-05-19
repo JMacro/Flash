094f808133206943a5e52d3807e0c0e686020570
JMacro <JMacro_H@163.com>
2020-05-19 22:15:15 +0800

    feat:新增hash负载均衡器

---
+++ Flash.LoadBalancer/Implements/HashLoadBalancer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flash.LoadBalancer
+{
+    /// <summary>
+    /// 哈希负载均衡器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class HashLoadBalancer<T> : ILoadBalancer<T>
+    {
+        private readonly Func<List<T>> _func;
+        private readonly object _lock = new object();
+        private int _index = 0;
+
+        public HashLoadBalancer(Func<List<T>> func)
+        {
+            this._func = func;
+        }
+
+        public T Resolve()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve(List<T> connections)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
