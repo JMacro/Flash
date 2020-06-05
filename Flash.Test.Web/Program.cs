9500df63a5b5b0a3a3da8115c52781ddfeb4a83d
林展鸿 <JMacro_H@163.com>
2020-06-05 13:46:51 +0800

    update

--- Flash.Test.Web/Program.cs
+++ Flash.Test.Web/Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-
+using System;
+using System.IO;
 
 namespace Flash.Test.Web
 {
@@ -16,23 +11,24 @@ namespace Flash.Test.Web
     {
         public static void Main(string[] args)
         {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Environment.SetEnvironmentVariable("Environment", env);
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseIISIntegration()
-            .UseKestrel()
-            .ConfigureAppConfiguration((context, config) =>
-            {
-                config.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFileEx("Config/appsettings.json", false, true)
-                .AddJsonFileEx("Config/metrics.json", false, true)
-                .AddJsonFileEx("Config/redis.json", false, true)
-                .AddEnvironmentVariables()
-                .AddCommandLine(args).Build();
-            })
-            .UseMetrics("AppMetrics")
-            .UseStartup<Startup>();
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFileEx("Config/appsettings.json", false, true)
+                    .AddJsonFileEx("Config/metrics.json", false, true)
+                    .AddJsonFileEx("Config/redis.json", false, true)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args).Build();
+                })
+                .UseMetrics("AppMetrics")
+                .UseStartup<Startup>();
     }
 }
--- Flash.Test.Web/Startup.cs
+++ Flash.Test.Web/Startup.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using Flash.Test.Web.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text;
 
 namespace Flash.Test.Web
 {
@@ -25,10 +20,9 @@ namespace Flash.Test.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddAutofac();
             services.AddMetrics(Configuration.GetSection("AppMetrics"));
             services.AddFlash(flash =>
@@ -66,24 +60,12 @@ namespace Flash.Test.Web
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            //var eventBus = app.ApplicationServices.GetRequiredService<Extersions.RabbitMQ.IBus>();
-            app.UseFlash(flash =>
-            {
-                //flash.UseRabbitMQ(rabbitmq =>
-                //{
-                //    rabbitmq.Register<Controllers.OrderInfo, OrderInfoHandler>(queueName: typeof(OrderInfoHandler).FullName);
-                //});
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
