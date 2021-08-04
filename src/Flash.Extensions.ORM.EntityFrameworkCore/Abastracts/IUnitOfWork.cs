3327b7f989d56677be8b4d6e2cd30702e1dfb93c
JMacro <JMacro_H@163.com>
2021-08-04 23:25:38 +0800

    EF工作单元新增事务提交回滚统一方法

--- src/Flash.Extensions.ORM.EntityFrameworkCore/Abastracts/IUnitOfWork.cs
+++ src/Flash.Extensions.ORM.EntityFrameworkCore/Abastracts/IUnitOfWork.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
 using System.Data;
+using System;
 using System.Threading.Tasks;
 
 namespace Flash.Extensions.ORM.EntityFrameworkCore
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
         bool SaveChanges();
         Task<bool> SaveChangesAsync();
         IDbContextTransaction BeginTransaction();
         IDbConnection GetConnection();
+        bool Commit();
+        Task<bool> CommitAsync();
     }
 }
--- src/Flash.Extensions.ORM.EntityFrameworkCore/Implements/UnitOfWork.cs
+++ src/Flash.Extensions.ORM.EntityFrameworkCore/Implements/UnitOfWork.cs
@@ -9,9 +9,9 @@ namespace Flash.Extensions.ORM.EntityFrameworkCore
     public class UnitOfWork : IUnitOfWork
     {
         private readonly DbContext _context;
-
+        private IDbContextTransaction _dbContextTransaction;
         private bool _disposed = false;
-        private bool _isTransaction = false;
+        public bool IsTransaction { get; private set; }
 
         public UnitOfWork(DbContext context)
         {
@@ -20,26 +20,19 @@ namespace Flash.Extensions.ORM.EntityFrameworkCore
 
         public bool SaveChanges()
         {
-            if (!_isTransaction)
-            {
-                return _context.SaveChanges() > 0;
-            }
-            return true;
+            return _context.SaveChanges() > 0;
         }
 
         public async Task<bool> SaveChangesAsync()
         {
-            if (!_isTransaction)
-            {
-                return await _context.SaveChangesAsync() > 0;
-            }
-            return true;
+            return await _context.SaveChangesAsync() > 0;
         }
 
         public IDbContextTransaction BeginTransaction()
         {
-            this._isTransaction = true;
-            return _context.Database.BeginTransaction();
+            this.IsTransaction = true;
+            this._dbContextTransaction = _context.Database.BeginTransaction();
+            return this._dbContextTransaction;
         }
 
         public void Dispose()
@@ -55,12 +48,46 @@ namespace Flash.Extensions.ORM.EntityFrameworkCore
                 _context.Dispose();
             }
 
+            if (this.IsTransaction && _dbContextTransaction != null)
+            {
+                this.IsTransaction = false;
+                this._dbContextTransaction.Dispose();
+            }
+
             _disposed = true;
         }
 
         public IDbConnection GetConnection()
         {
             return _context.Database.GetDbConnection();
+        }
+
+        public bool Commit()
+        {
+            try
+            {
+                this._dbContextTransaction.Commit();
+                return true;
+            }
+            catch (Exception)
+            {
+                this._dbContextTransaction.Rollback();
+                return false;
+            }
+        }
+
+        public async Task<bool> CommitAsync()
+        {
+            try
+            {
+                await this._dbContextTransaction.CommitAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                await this._dbContextTransaction.RollbackAsync();
+                return false;
+            }
         }
     }
 }
