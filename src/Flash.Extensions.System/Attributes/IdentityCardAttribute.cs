14a7e81d20bd545be68e91add56b641509ab2c9a
JMacro <JMacro_H@163.com>
2021-08-20 12:04:12 +0800

    新增身份证号校验及特性

---
+++ src/Flash.Extensions.System/Attributes/IdentityCardAttribute.cs
@@ -0,0 +1,26 @@
+﻿#if NETCORE
+namespace System.ComponentModel.DataAnnotations
+{
+    /// <summary>
+    /// 身份证验证
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = true)]
+    public sealed class IdentityCardAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            string valueAsString = value as string;
+            return valueAsString != null && valueAsString.ValidCardId();
+        }
+    }
+}
+#endif
+
+#if NET45
+
+#endif
--- src/Flash.Extensions.System/StringExtensions.cs
+++ src/Flash.Extensions.System/StringExtensions.cs
@@ -299,5 +299,51 @@ namespace System
                 return string.Join("", array);
             }
         }
+
+        /// <summary>
+        /// 身份证号校验
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool ValidCardId(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return false;
+            }
+
+            long n = 0;
+            if (long.TryParse(value.Remove(17), out n) == false
+                || n < Math.Pow(10, 16) || long.TryParse(value.Replace('x', '0').Replace('X', '0'), out n) == false)
+            {
+                return false;//数字验证  
+            }
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+            if (address.IndexOf(value.Remove(2)) == -1)
+            {
+                return false;//省份验证  
+            }
+            string birth = value.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+            DateTime time = new DateTime();
+            if (DateTime.TryParse(birth, out time) == false)
+            {
+                return false;//生日验证  
+            }
+            string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+            string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+            char[] Ai = value.Remove(17).ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+            }
+            int y = -1;
+            Math.DivRem(sum, 11, out y);
+            if (arrVarifyCode[y] != value.Substring(17, 1).ToLower())
+            {
+                return false;//校验码验证  
+            }
+            return true;//符合GB11643-1999标准
+        }
     }
 }
