10617603edaa2c9f3cbe22c395f1e12140bc3ce0
JMacro <JMacro_H@163.com>
2021-08-25 16:19:41 +0800

    新增标签标记注解

---
+++ src/Flash.Extensions.System/Attributes/AutoMapperToAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace System
+{
+    /// <summary>
+    /// 实体映射标签
+    /// </summary>
+    public class AutoMapperToAttribute : Attribute
+    {
+        private readonly Type[] _types;
+
+        /// <summary>
+        /// 实体映射
+        /// </summary>
+        /// <param name="types">需映射的类型</param>
+        public AutoMapperToAttribute(params Type[] types)
+        {
+            this._types = types;
+        }
+
+        /// <summary>
+        /// 获得映射的类型
+        /// </summary>
+        /// <returns></returns>
+        public Type[] GetMapperList()
+        {
+            return this._types;
+        }
+    }
+}
---
+++ src/Flash.Extensions.System/Attributes/DataTypeAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace System
+{
+    /// <summary>
+    /// 数据类型标记
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = true)]
+    public class DataTypeAttribute : Attribute
+    {
+        private readonly Type _type;
+
+        /// <summary>
+        /// 数据类型标记
+        /// </summary>
+        /// <param name="type">需标记的类型</param>
+        public DataTypeAttribute(Type type)
+        {
+            this._type = type;
+        }
+
+        /// <summary>
+        /// 获得标记的类型
+        /// </summary>
+        /// <returns></returns>
+        public Type GetDataType()
+        {
+            return this._type;
+        }
+    }
+}
