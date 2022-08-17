f154e309ef6162715906398cdff86fa05d319273
JMacro <JMacro_H@163.com>
2022-08-17 10:33:24 +0800

    .

--- build-nuget.cmd
+++
@@ -1,9 +0,0 @@
-set workDir=%cd%
-set nupkgs_path=%workDir%\nupkgs
-echo "Building..."
-for /d %%i in (Flash.*) do (
-	dotnet pack %workDir%\%%i --output %nupkgs_path%	
-)
-echo "Build End"
-pause
-start explorer %nupkgs_path%
